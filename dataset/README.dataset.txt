854e5ae9093c67aae8dc378dc37c0d5fcd3595d7
Kiet Hoang Gia <kiethoang101@gmail.com>
2025-06-10 22:45:44 +0700

    feat: train model

---
+++ dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# convert_yolo_2_voc > 2025-04-02 10:48am
+https://universe.roboflow.com/convertyolo2voc/convert_yolo_2_voc
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,20 @@
+import torch
+from ultralytics import YOLO
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Use {device}')
+
+if __name__ == '__main__':
+    model = YOLO('./yolo11l.pt')
+    
+    results = model.train(
+        data='dataset/data.yaml',
+        epochs=10,  
+        device=device,
+        batch=4,
+        workers=2,
+        save_period=2,
+        project='runs/train',
+        name='v11-l',
+        # resume=True
+    )
